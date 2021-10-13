f19f227cba5ed7be254d3e3ae5d73e6408a8cc7c
wangyunlai.wyl <wangyunlai.wyl@antgroup.com>
2021-10-13 10:30:05 +0800

    init

--- build_local.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p bin && mvn clean package -Dmaven.test.skip=true && cp server/target/logproxy-jar-with-dependencies.jar ./bin/logproxy.jar && cp client/target/logproxy-client.jar ./bin/logproxy-client.jar && cp ./server/script/* ./bin && cp ./server/script/run.sh .
